9db1a2111775f4007d9083594bf54dcd059a0ce9
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-18 11:54:55 +0900

    emaillist #1

---
+++ mysqlclient-practices/emaillist/app.py
@@ -0,0 +1,18 @@
+cmd = input('(l)ist, (a)dd, (d)elete, (q)uit > ')
+
+while True:
+    cmd = input('(l)ist, (a)dd, (d)elete, (q)uit > ')
+
+    if cmd == 'q':
+        break
+    elif cmd == 'l':
+        print('list 처리')
+    elif cmd == 'a':
+        print('add 처리')
+    elif cmd == 'd':
+        print('delete 처리')
+    else:
+        print('알 수 없는 명령 입니다.')
+
+
+print(cmd)
