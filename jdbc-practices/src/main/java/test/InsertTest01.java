91f04e0fdc22edf1302d501019cfef89a385e80f
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-04 09:49:51 +0900

    preparedstatement

--- jdbc-practices/src/main/java/test/InsertTest01.java
+++ jdbc-practices/src/main/java/test/InsertTest01.java
@@ -2,43 +2,43 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class SelectTest {
+public class InsertTest01 {
 
 	public static void main(String[] args) {
-		search("pat");
-
+		insert("시스템");
+		insert("마케팅");
+		insert("운영");
 	}
 
-	private static void search(String keyword) {
+	private static Boolean insert(String name) {
+		boolean result = false;
+		
 		Connection conn = null;
 		Statement stmt = null;
-		ResultSet rs = null;
+		
 		try {
 			//1. JDBC Driver Class Loading
 			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8";
-			conn = DriverManager.getConnection(url, "hr", "hr");
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 			//3. Statement 생성
 			stmt = conn.createStatement();
 			
 			//4. SQL 실행
-			String sql = "select emp_no, first_name" +
-						 " from employees" +
-						 " where first_name like '%" + keyword + "%'";
-			 rs = stmt.executeQuery(sql);
-			while(rs.next()) {
-				Long empNo = rs.getLong(1);
-				String firstName = rs.getString(2);
-				
-				System.out.println(empNo + ":" + firstName);
-			}
+			String sql = 
+				" insert" +
+				"   into dept" +
+				" values (null, '" + name + "')";
+			
+			int count = stmt.executeUpdate(sql);
+			
+			result = count == 1;
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -46,21 +46,18 @@ public class SelectTest {
 			System.out.println("Error:" + e);
 		} finally {
 			try {
-				if(rs != null) {
-					rs.close();
-				}
 				if(stmt != null) {
 					stmt.close();
 				}
-				if(conn != null) { 
-				conn.close();
+				
+				if(conn != null) {
+					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		
+		return result;
 	}
-		
-	}
-
+}
--- jdbc-practices/src/main/java/test/SelectTest01.java
+++ jdbc-practices/src/main/java/test/SelectTest01.java
@@ -2,45 +2,43 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UpdateTest {
+public class SelectTest01 {
 
 	public static void main(String[] args) {
-		DeptVo vo = new DeptVo();
-		vo.setNo(1L);
-		vo.setName("경영지원");
-		
-		boolean result = update(vo);
-		System.out.println(result ? "성공" : "실패");
+		search("pat");
+
 	}
 
-	private static boolean update(DeptVo deptVo) {
-		boolean result = false;
+	private static void search(String keyword) {
 		Connection conn = null;
 		Statement stmt = null;
-		
+		ResultSet rs = null;
 		try {
 			//1. JDBC Driver Class Loading
 			Class.forName("org.mariadb.jdbc.Driver");
 			
 			//2. 연결하기
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			String url = "jdbc:mysql://127.0.0.1:3306/employees?charset=utf8";
+			conn = DriverManager.getConnection(url, "hr", "hr");
 			
 			//3. Statement 생성
 			stmt = conn.createStatement();
 			
 			//4. SQL 실행
-			String sql = 
-				"update dept" +
-				"   set name = '" + deptVo.getName() + "'" +
-			    " where no = " + deptVo.getNo();
-			
-			int count = stmt.executeUpdate(sql);
-			
-			result = count == 1;
+			String sql = "select emp_no, first_name" +
+						 " from employees" +
+						 " where first_name like '%" + keyword + "%'";
+			 rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				Long empNo = rs.getLong(1);
+				String firstName = rs.getString(2);
+				
+				System.out.println(empNo + ":" + firstName);
+			}
 			
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패:" + e);
@@ -48,21 +46,21 @@ public class UpdateTest {
 			System.out.println("Error:" + e);
 		} finally {
 			try {
+				if(rs != null) {
+					rs.close();
+				}
 				if(stmt != null) {
 					stmt.close();
 				}
-				
-				if(conn != null) {
-					conn.close();
+				if(conn != null) { 
+				conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		
-		return result;
 	}
-	
-	
+		
+	}
 
-}
